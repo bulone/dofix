f8bf5ba9c91ec579a8c2dc350e30950f366f680a
bulone <mxsix112@gmail.com>
2025-03-26 17:41:11 +0800

    update 3 files and create 2 files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,18 @@
-msemoji.parse(document.body,{
-    base: "https://raw.gitmirror.com/DellZHackintosh/msemoji/refs/heads/main/src/",
-    ext: ".svg",
-    folder: "Color",
-    high_contrast: false
-  });
+/* ------------------ MSEmoji ----------------- */
+msemoji.parse(document.body, {
+  base: "https://raw.gitmirror.com/DellZHackintosh/msemoji/refs/heads/main/src/",
+  ext: ".svg",
+  folder: "Color",
+  high_contrast: false
+});
+
+/* ------------------ 跳转风险提示 ------------------ */
+document.body.addEventListener('click', function(e) {
+  let target = e.target.closest('.wl-cards a,.card-link');
+  if (target && !target.href.includes('bulone.github.io')) {
+      e.preventDefault();
+      let encodedUrl = btoa(target.href);
+      let url = '/redirect?url=' + encodedUrl;
+      window.open(url, '_blank');
+  }
+});
